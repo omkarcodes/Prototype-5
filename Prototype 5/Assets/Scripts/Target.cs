b3b603da84c2d43322cc0859ca2041e711ecc32e
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-03-16 23:28:44 +0530

    created prefabs and added them force and torque

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        ApplyForce();
+        ApplyTorque();
+        SetPosition();
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ApplyForce()
+    {
+        int speed = Random.Range(12, 18);
+        targetRb.AddForce(Vector3.up * speed, ForceMode.Impulse);
+    }
+
+    void ApplyTorque()
+    {
+        int range = Random.Range(-10, 10);
+        targetRb.AddTorque(range, range, range);
+    }
+
+    void SetPosition()
+    {
+        transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+    }
+}
