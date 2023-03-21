c2d329b939aa74618f00297f8ce88e07a8b88f90
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2023-03-21 23:02:34 +0530

    added the gameplay to the game

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -6,38 +6,52 @@ public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
 
+    private float minForce = 12;
+    private float maxForce = 15;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = 2;
+
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(ApplyForce(), ForceMode.Impulse);
+        targetRb.AddTorque(ApplyTorque(), ApplyTorque(), ApplyTorque());
 
-        ApplyForce();
-        ApplyTorque();
-        SetPosition();
- 
+        transform.position = SetPosition();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    void ApplyForce()
+    private void OnMouseDown()
     {
-        int speed = Random.Range(12, 18);
-        targetRb.AddForce(Vector3.up * speed, ForceMode.Impulse);
+        Destroy(gameObject);
     }
 
-    void ApplyTorque()
+    private void OnTriggerEnter(Collider other)
     {
-        int range = Random.Range(-10, 10);
-        targetRb.AddTorque(range, range, range);
+        Destroy(gameObject);
+    }
+    Vector3 ApplyForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
     }
 
-    void SetPosition()
+    float ApplyTorque()
     {
-        transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 SetPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), -ySpawnPos, 0);
     }
 }
